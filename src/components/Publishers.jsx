4286bf990ae5cd7725246c591a117480aae575f1
unknown <elhanhajrovic02@gmail.com>
2025-09-15 22:10:51 +0200

    hotfix: publishers loading problem.

--- src/components/Publishers.jsx
+++ src/components/Publishers.jsx
@@ -5,48 +5,52 @@ const Publishers = () => {
     const [publishers, setPublishers] = useState([]);
     const [errorMsg, setErrorMsg] = useState('');
 
-    useEffect((
-        async function loadPublishers() {
-            try {
-                const data = await getAllPublishers();
-                setPublishers(data);
-            } catch (error) {
-                if (error.status && error.status === 500) {
-                    setErrorMsg("We're experiencing technical difficulties. Please try again shortly.")
-                } else if (error.request) {
-                    setErrorMsg("The server seems to be taking too long to respond. Please try again in a moment.");
-                } else {
-                    setErrorMsg("Something went wrong. Please try again.");
-                }
-                console.log("An error occurred while fetching Publishers:", error);
+    async function loadPublishers() {
+        try {
+            const data = await getAllPublishers();
+            setPublishers(data);
+        } catch (error) {
+            if (error.status && error.status === 500) {
+                setErrorMsg("We're experiencing technical difficulties. Please try again shortly.")
+            } else if (error.request) {
+                setErrorMsg("The server seems to be taking too long to respond. Please try again in a moment.");
+            } else {
+                setErrorMsg("Something went wrong. Please try again.");
             }
-            
-            loadPublishers();
+            console.log("An error occurred while fetching Publishers:", error);
         }
-    ), []);
-    return (
-        <div className="publishers">
-            <h1>Publishers page</h1>
-            <table>
-                <thead>
-                    <tr>
-                        <th>Name</th>
-                        <th>Address</th>
-                        <th>Website</th>
+    }
+
+    useEffect(() => {
+        loadPublishers ();
+
+        return (
+            console.log('Publishers unmounted')
+        );
+    }, []);
+return (
+    <div className="publishers">
+        <h1>Publishers page</h1>
+        <table>
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Address</th>
+                    <th>Website</th>
+                </tr>
+            </thead>
+            <tbody>
+                {publishers.map(publisher => (
+                    <tr key={publisher.id}>
+                        <td>{publisher.name}</td>
+                        <td>{publisher.address}</td>
+                        <td>{publisher.website}</td>
                     </tr>
-                </thead>
-                <tbody>
-                    {publishers.map( publisher => (
-                        <tr key={publisher.id}>
-                            <td>{publisher.name}</td>
-                            <td>{publisher.address}</td>
-                            <td>{publisher.website}</td>
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
-        </div>
-    )
+                ))}
+            </tbody>
+        </table>
+    </div>
+)
 }
 
 export default Publishers;
